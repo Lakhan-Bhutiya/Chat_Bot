60db5ef6300501ec571e1ecdb1093bb5daab624c
Lakhan-Bhutiya <bhutiyalakhan13@gmail.com>
2026-02-22 16:50:32 +0530

    Add date_and_time field and show IST timestamp in chat

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class QuestionAnswer(models.Model):
     question = models.TextField()
     answer = models.TextField()
+    date_and_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.question
